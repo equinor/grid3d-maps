23351cd4b12810c78ec6f280832e9f92d9ea6699
Sondre Sortland <sonso@equinor.com>
2020-06-02 10:06:02 +0200

    Fix use of __version__

--- src/xtgeoapp_grd3dmaps/__init__.py
+++ src/xtgeoapp_grd3dmaps/__init__.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """Top-level package for xtgeoapp_grd3dmaps"""
+
+try:
+    from ._theversion import version
+
+    __version__ = version
+except ImportError:
+    __version__ = "0.0.0"
