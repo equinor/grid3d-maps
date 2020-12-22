0ae726de1ee35d2a469a7dcf4cefc7af95685034
Håvard Berland <havb@equinor.com>
2020-12-22 09:47:41 +0100

    Fix ERT forward models in CI

--- tests/test_ert_hooks.py
+++ tests/test_ert_hooks.py
@@ -1,26 +1,67 @@
-import os
-import sys
 import subprocess
+from pathlib import Path
+
 import yaml
 
-import pytest
 
-try:
-    import ert_shared  # noqa
-except ImportError:
-    pytest.skip(
-        "ERT is not installed, skipping hook implementation tests.",
-        allow_module_level=True,
-    )
+TESTDIR = Path(__file__).absolute().parent
+XTGEOTESTDATA = TESTDIR.parent.parent / "xtgeo-testdata"
 
-TESTDIR = os.path.dirname(os.path.abspath(__file__))
+assert (
+    XTGEOTESTDATA.is_dir()
+), "Please clone xtgeo-testdata alongside this repo for test to run"
 
 
-@pytest.mark.skipif(sys.version_info < (3, 6), reason="requires python3.6 or higher")
 def test_xtgeoapps_through_ert(tmpdir):
     tmpdir.chdir()
 
     eclbase = "REEK"
+
+    # Configuration file for grid3d_average_map,
+    # based on tests/yaml/avg1f.yml
+    avg_conf = {
+        "title": "Reek",
+        "input": {
+            "folderroot": str(XTGEOTESTDATA / "3dgrids" / "reek"),
+            "grid": "$folderroot/reek2_grid_w_zerolayer.roff",
+            "dz": "$folderroot/reek2_grid_w_zerolayer_dz.roff",
+        },
+        "zonation": {"zranges": [{"ZERO": [15, 15]}]},
+        "plotsettings": {
+            "faultpolygons": str(
+                XTGEOTESTDATA
+                / "polygons"
+                / "reek"
+                / "1"
+                / "top_upper_reek_faultpoly.xyz"
+            ),
+            "dz": {"valuerange": [0, 3]},
+        },
+        "computesettings": {
+            "zone": True,
+            "all": False,
+            "tuning": {"zone_avg": True, "coarsen": 2},
+        },
+        "output": {"tag": "avg1f", "mapfolder": ".", "plotfolder": "."},
+    }
+    Path("avg_conf.yml").write_text(yaml.dump(avg_conf))
+
+    # Configuration file for grid3d_thickness,
+    # based on tests/yaml/avg1f.yml
+    eclroot = str(XTGEOTESTDATA / "3dgrids" / "reek" / "REEK")
+    # (eclroot cannot be only in yaml, it is overriden by the forward model
+    # default argument)
+    hc_conf = {
+        "title": "Reek",
+        "input": {
+            "grid": str(
+                XTGEOTESTDATA / "3dgrids" / "reek" / "reek_grid_fromegrid.roff"
+            ),
+            "dates": ["19991201"],
+        },
+        "output": {"tag": "hc1f", "mapfolder": ".", "plotfolder": "."},
+    }
+    Path("hc_conf.yml").write_text(yaml.dump(hc_conf))
 
     ert_config = [
         "ECLBASE " + eclbase + ".DATA",
@@ -28,23 +69,20 @@ def test_xtgeoapps_through_ert(tmpdir):
         "NUM_REALIZATIONS 1",
         "RUNPATH .",
     ]
+    # Path("tmp").mkdir()
 
-    # This is not a full test env of grid3d_* tools, just the
-    # installation of the forward models, so we will provide a dummy
-    # yaml input making them fail.
-    conf = {"title": "Reek", "input": 0}
-    with open("conf.yml", "w") as file_h:
-        file_h.write(yaml.dump(conf))
+    ert_config.append("FORWARD_MODEL GRID3D_AVERAGE_MAP(<CONFIG_AVGMAP>=avg_conf.yml)")
+    ert_config.append(
+        "FORWARD_MODEL GRID3D_HC_THICKNESS(<CONFIG_HCMAP>=hc_conf.yml, <ECLROOT>="
+        + eclroot
+        + ")"
+    )
 
-    ert_config.append("FORWARD_MODEL GRID3D_AVERAGE_MAP(<CONFIG_HCMAP>=conf.yml)")
-    ert_config.append("FORWARD_MODEL GRID3D_HC_THICKNESS(<CONFIG_HCMAP>=conf.yml)")
-
-    ert_config_filename = "xtgeoapps_test.ert"
-    with open(ert_config_filename, "w") as file_h:
-        file_h.write("\n".join(ert_config))
+    ert_config_filename = Path(tmpdir) / "xtgeoapps_test.ert"
+    ert_config_filename.write_text("\n".join(ert_config))
 
     subprocess.call(["ert", "test_run", ert_config_filename])
 
-    # ERT will only get here if the jobs exists and are configured. There
-    # is no OK file since the jobs did fail.
-    assert os.path.exists("EXIT")
+    assert Path("zero--avg1f_average_dz.gri").is_file()
+    assert Path("all--hc1f_oilthickness--19991201.png").is_file()
+    assert Path("OK").is_file()
