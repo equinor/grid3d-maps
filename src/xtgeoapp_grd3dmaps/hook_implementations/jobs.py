c09565103849e6d356e2dd94cf5116260d2429f0
Sondre Sortland <sonso@equinor.com>
2022-08-18 07:21:34 +0200

    Migrate to new ert import structure

--- src/xtgeoapp_grd3dmaps/hook_implementations/jobs.py
+++ src/xtgeoapp_grd3dmaps/hook_implementations/jobs.py
@@ -2,8 +2,12 @@ import importlib
 import os
 from pkg_resources import resource_filename
 
-from ert_shared.plugins.plugin_manager import hook_implementation
-from ert_shared.plugins.plugin_response import plugin_response
+try:
+    from ert_shared.plugins.plugin_manager import hook_implementation
+    from ert_shared.plugins.plugin_response import plugin_response
+except ModuleNotFoundError:
+    from ert.shared.plugins.plugin_manager import hook_implementation
+    from ert.shared.plugins.plugin_response import plugin_response
 
 PLUGIN_NAME = "xtgeoapp_grd3dmaps"
 
