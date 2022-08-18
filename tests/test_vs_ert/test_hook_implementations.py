d2029ef2f2b383069f837cd0558330cefd004059
Sondre Sortland <sonso@equinor.com>
2022-08-18 07:21:34 +0200

    Migrate to new ert import structure

--- tests/test_vs_ert/test_hook_implementations.py
+++ tests/test_vs_ert/test_hook_implementations.py
@@ -8,12 +8,18 @@ import pytest
 try:
     import ert_shared  # noqa
 except ImportError:
-    pytest.skip(
-        "Not testing ERT hooks when ERT is not installed", allow_module_level=True
-    )
+    try:
+        import ert # noqa
+    except ImportError:
+        pytest.skip(
+            "Not testing ERT hooks when ERT is not installed", allow_module_level=True
+        )
 
 import xtgeoapp_grd3dmaps.hook_implementations.jobs as jobs
-from ert_shared.plugins.plugin_manager import ErtPluginManager
+try:
+    from ert_shared.plugins.plugin_manager import ErtPluginManager
+except ModuleNotFoundError:
+    from ert.shared.plugins.plugin_manager import ErtPluginManager
 
 EXPECTED_JOBS = {
     "GRID3D_AVERAGE_MAP": "xtgeoapp_grd3dmaps/config_jobs/GRID3D_AVERAGE_MAP",
