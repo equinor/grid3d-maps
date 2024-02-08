769793730ce966ca7a0a6fe78e6d6bc2883e8e8b
jorgesicachanr <114475076+jorgesicachanr@users.noreply.github.com>
2024-02-08 14:45:54 +0100

    Update _config.py

--- src/xtgeoapp_grd3dmaps/aggregate/_config.py
+++ src/xtgeoapp_grd3dmaps/aggregate/_config.py
@@ -6,7 +6,7 @@ explicitly provided.
 """
 from dataclasses import dataclass, field
 from enum import Enum
-from typing import Dict, List, Optional, Tuple, Union
+from typing import Dict, List, Optional, Tuple
 
 
 class AggregationMethod(Enum):
