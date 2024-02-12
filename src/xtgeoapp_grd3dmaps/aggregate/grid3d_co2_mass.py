5b7bd877a375636219fc8dd293c18fdd956698a3
Jorge Sicacha <sicacha@nr.no>
2024-02-12 16:50:19 +0100

    Few more changes to grid3d_co2_mass.py

--- src/xtgeoapp_grd3dmaps/aggregate/grid3d_co2_mass.py
+++ src/xtgeoapp_grd3dmaps/aggregate/grid3d_co2_mass.py
@@ -4,7 +4,7 @@ import sys
 import tempfile
 import xtgeo
 import yaml
-from typing import List, Optional
+from typing import List, Optional, Dict, Tuple
 from xtgeoapp_grd3dmaps.aggregate import (
     _co2_mass,
     _config,
@@ -45,20 +45,10 @@ PROPERTIES_TO_EXTRACT = [
 def generate_co2_mass_maps(config_) :
 
     """
-    #-> List[List[xtgeo.GridProperty]]:
-
-    Calculates and exports 3D CO2 mass properties from the provided grid and config files
+    Calculates and exports 2D and 3D CO2 mass properties from the provided config file
 
     Args:
-        grid_file (str): Path to EGRID-file
-        co2_mass_settings (CO2MassSettings): Settings from config file for calculation
-                                             of CO2 mass maps.
-        out_folder (str): Path to store the produced 3D GridProperties.
-
-
-    Returns:
-        List[List[xtgeo.GridProperty].
-
+        config_: Arguments in the config file
     """
 
     co2_mass_settings = config_.co2_mass_settings
@@ -66,14 +56,11 @@ def generate_co2_mass_maps(config_) :
     zones = co2_mass_settings.zones
     if zones is not None and isinstance(zones, str):
         co2_mass_settings.zones = [zones]
-
     grid_file = config_.input.grid
-    co2_data = calculate_co2(grid_file,co2_mass_settings.unrst_source,"mass",co2_mass_settings.init_source,None)    
-
+    co2_data = calculate_co2(grid_file,co2_mass_settings.unrst_source,"mass",co2_mass_settings.init_source,None)
     dates = config_.input.dates
     if len(dates)>0:
         co2_data.data_list = [x for x in co2_data.data_list if x.date in dates]
-
     out_property_list = _co2_mass.translate_co2data_to_property(
         co2_data,
         grid_file,
@@ -81,29 +68,15 @@ def generate_co2_mass_maps(config_) :
         PROPERTIES_TO_EXTRACT,
         config_.output.mapfolder + "/grid",
     )
-
     config_.zonation.zranges, all_zrange = process_zonation(co2_mass_settings,zonation,grid_file)
-
-    """
-    if config_.computesettings.all :
-        if len(zonation.zranges)>0:             
-            all_zrange = find_all_zrange(zonation=zonation)
-        else:
-            all_zrange = find_all_zrange(grid_file=grid_file)
-    """
-
     if len(config_.zonation.zranges)>0:
         config_.zonation.zproperty = None
-
-
-
     if config_.computesettings.all:
         config_.zonation.zranges.append({'all':all_zrange})
         config_.computesettings.all = False
         if not config_.computesettings.zone:
             config_.computesettings.zone = True
             config_.zonation.zranges = [zrange for zrange in config_.zonation.zranges if 'all' in zrange]
-
     co2_mass_property_to_map(config_,out_property_list)
 
 def co2_mass_property_to_map(
@@ -111,114 +84,56 @@ def co2_mass_property_to_map(
     property_list: List[xtgeo.GridProperty],
 ):
     """
-    Aggregates with SUM and writes a CO2 mass property to file using `grid3d_aggregate_map`.
-    The property is written to a temporary file while performing the
-    aggregation.
+    Aggregates with SUM and writes a list of CO2 mass property to files
+    using `grid3d_aggregate_map`.
 
     Args:
         config_: Arguments in the config file
-        t_prop: Grid property to be aggregated
+        property_list: List of Grid property objects to be aggregated
 
     """
     config_.input.properties = []
     config_.computesettings.aggregation = _config.AggregationMethod.SUM
     config_.output.aggregation_tag = False
-
     for props in property_list:
         if len(props)>0 :
             for prop in props:
-                config_.input.properties.append(_config.Property(config_.output.mapfolder+"/grid/"+prop.name+"--"+prop.date+".roff", None, None))
-                
+                config_.input.properties.append(_config.Property(config_.output.mapfolder+
+                                                                 "/grid/"+prop.name+"--"+
+                                                                 prop.date+".roff", None, None))
     grid3d_aggregate_map.generate_from_config(config_)
 
-"""
-def define_zones_to_plot(
-        zonation: _config.Zonation,
-        co2_mass_settings: _config.CO2MassSettings,
-):
-
-    if len(zonation.zranges) > 0 :
-        zone_names = [list(item.keys())[0] for item in zonation.zranges]    
-    elif zonation.zproperty is not None :
-        if zonation.zproperty.source.split(".")[-1] in ["yml", "yaml"]:
-            with open(zonation.zproperty.source, "r", encoding="utf8") as stream:
-                try:
-                    zfile = yaml.safe_load(stream)
-                except yaml.YAMLError as exc:
-                    print(exc)
-                    sys.exit()
-            if "zranges" not in zfile:
-                error_text = "The yaml zone file must be in the format:\nzranges:\
-                \n    - Zone1: [1, 5]\n    - Zone2: [6, 10]\n    - Zone3: [11, 14])"
-                raise Exception(error_text) 
-            zonation.zranges = zfile['zranges']
-            zone_names = [list(item.keys())[0] for item in zonation.zranges]    
-        
-
-    if co2_mass_settings.zones is not None:
-        zones_to_plot = [zone for zone in co2_mass_settings.zones if zone in zone_names]
-        if len(zones_to_plot)==0:
-            print("The zones specified in CO2 mass settings are not part of the zonation provided \n maps will be exported for all the existing zones")
-        else:
-            return([item for item in zonation.zranges if list(item.keys())[0] in zones_to_plot])
-    else:
-        return(zonation.zranges)
-"""
-"""
-def find_all_zrange(
-        zonation: Optional[_config.Zonation] = None,
-        grid_file: Optional[str] = None,
-):
-    if zonation is not None:
-        if len(zonation.zranges) > 0:
-            zranges_limits = [list(d.values())[0] for d in zonation.zranges]
-        elif zonation.zproperty is not None:
-            if zonation.zproperty.source.split(".")[-1] in ["yml", "yaml"]:
-                with open(zonation.zproperty.source, "r", encoding="utf8") as stream:
-                    try:
-                        zfile = yaml.safe_load(stream)
-                    except yaml.YAMLError as exc:
-                        print(exc)
-                        sys.exit()
-                if "zranges" not in zfile:
-                    error_text = "The yaml zone file must be in the format:\nzranges:\
-                    \n    - Zone1: [1, 5]\n    - Zone2: [6, 10]\n    - Zone3: [11, 14])"
-                    raise Exception(error_text)            
-            zranges_limits = [list(d.values())[0] for d in zfile.zranges]
-    elif grid_file is not None:
-        grid_pf = xtgeo.grid_from_file(grid_file)
-        dimensions = (grid_pf.ncol, grid_pf.nrow, grid_pf.nlay)
-        zranges_limits = [[1,grid_pf.nlay]]
-    else:
-        error_text = "Either zonation or grid_file need to be provided"
-        raise Exception(error_text)            
-
-    max_zvalue = max(sublist[-1] for sublist in zranges_limits)
-    min_zvalue = min(sublist[0] for sublist in zranges_limits)
-    all_zrange = [min_zvalue, max_zvalue]
-    return(all_zrange)                      
-"""
 def process_zonation(co2_mass_settings: _config.CO2MassSettings,
-                     zonation: Optional[_config.Zonation]=None,
-                     grid_file: Optional[str] = None):
-    if zonation is not None:
+                     grid_file: str,
+                     zonation: Optional[_config.Zonation]=None
+                     ) -> Tuple[List,List]:
+    """
+    Processes a zonation file, if existing, and extracts both zranges per zone
+    and the complete range in the zaxis. Otherwise, uses the grid_file.
+
+    Args:
+        co2_mass_settings: Arguments in CO2 mass settings
+        grid_file: Path to grid file
+        zonation: Arguments in zonation
+
+    Returns:
+        Tuple[List,List]
+    """
+    if zonation.zproperty is not None or len(zonation.zranges)>0:
         if zonation.zproperty is not None:
             if zonation.zproperty.source.split(".")[-1] in ["yml", "yaml"]:
                 zfile = read_yml_file(zonation.zproperty.source)
-                zonation.zranges = zfile['ranges']
+                zonation.zranges = zfile['zranges']
         if len(zonation.zranges) > 0:
             zone_names = [list(item.keys())[0] for item in zonation.zranges]
             zranges_limits = [list(d.values())[0] for d in zonation.zranges]
-    elif grid_file is not None:
+    else:
         grid_pf = xtgeo.grid_from_file(grid_file)
         zranges_limits = [[1,grid_pf.nlay]]
-    else:
-        error_text = "Either zonation or grid_file need to be provided"
-        raise Exception(error_text)
+        zone_names = None
     max_zvalue = max(sublist[-1] for sublist in zranges_limits)
     min_zvalue = min(sublist[0] for sublist in zranges_limits)
     all_zrange = [min_zvalue, max_zvalue]
-
     if zone_names is not None:
         if co2_mass_settings.zones is not None:
             zones_to_plot = [zone for zone in co2_mass_settings.zones if zone in zone_names]
@@ -233,7 +148,10 @@ def process_zonation(co2_mass_settings: _config.CO2MassSettings,
     else:
         return [], all_zrange
 
-def read_yml_file(file_path: str):
+def read_yml_file(file_path: str) -> Dict[List]:
+    """
+    Reads a yml from a given path in file_path argument
+    """
     with open(file_path, "r", encoding="utf8") as stream:
         try:
             zfile = yaml.safe_load(stream)
@@ -249,7 +167,7 @@ def read_yml_file(file_path: str):
 def main(arguments=None):
     """
     Takes input arguments and calculates co2 mass as a property and aggregates it to a 2D map
-    at each time step, divided into different phases and locations(TODO).
+    at each time step, divided into different phases and locations.
     """
     if arguments is None:
         arguments = sys.argv[1:]
