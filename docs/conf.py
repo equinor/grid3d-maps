0fbe498deaa1bd2ac2e55cf9eb205190f3c37b53
mferrera <matt.ferrera@gmail.com>
2024-06-18 08:31:09 +0200

    DOC: Use myst-parser for README

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,7 @@ sys.path.insert(0, project_root)
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
+    "myst_parser",
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
